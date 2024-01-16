b5733b692a12c12aea9ce7bba7ba9cd8b61d9c41
prakash1926 <ananthaprakash71@gmail.com>
2024-01-16 16:21:37 +0530

    first commit

---
+++ BooksDataManagement/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BooksDataManagement.Models
+{
+    public partial class Author
+    {
+        public Author()
+        {
+            Books = new HashSet<book>();
+        }
+
+        public int AuthorId { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<book> Books { get; set; }
+    }
+}
---
+++ BooksDataManagement/Models/BookCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BooksDataManagement.Models
+{
+    public partial class BookCategory
+    {
+        public BookCategory()
+        {
+            Books = new HashSet<book>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+
+        public virtual ICollection<book> Books { get; set; }
+    }
+}
